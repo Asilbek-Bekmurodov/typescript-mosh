3835a32a469bc99a022a3a9cd4dbdce2918b5b5c
Asilbek2002 <school2924@gmail.com>
2023-02-12 02:06:31 +0500

    fundamentals

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// let sales = 123_456_789; // number
+// let course = "TypeScript"; // string
+// let is_published = true; // boolean
+// let number: number[] = [1, 2, 3]; // arrays
+// let arr: [number, string] = [1, "s"];  // tuples
+
+// PascalCase
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Big,
+// }
+
+// let mySize = Size.Medium;
+// console.log(mySize);
+
+// functions
+
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
+
+// calculateTax(10_000);
+
+// let employee: {
+//   readonly id: number;
+//   name?: string;
+//   retire: (date: Date) => void;
+// } = {
+//   id: 1,
+//   retire(date) {
+//     console.log(date);
+//   },
+// };
+
+// employee.id = 2;
